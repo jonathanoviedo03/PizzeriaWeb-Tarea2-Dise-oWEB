821af0675bc1447fb1100135cc04dcec1a839135
PC <PC@DESKTOP-DC6NM3N>
2022-06-06 20:30:55 -0600

    Comit del proyecto de la tarea 2 del curso de diseño web y patrones

---
+++ src/main/java/com/pizzeriaWeb/PizzeriaWebApplication.java
@@ -0,0 +1,13 @@
+package com.pizzeriaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pizzeriaWeb/PizzeriaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzeriaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
